6928298e2bb385b8b3b522387651dfe23471fbed
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-06-22 20:14:47 -0300

    Add factory e blueprint de autor

---
+++ database/factory.js
@@ -0,0 +1,24 @@
+﻿'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Factory
+|--------------------------------------------------------------------------
+|
+| Factories are used to define blueprints for database tables or Lucid
+| models. Later you can use these blueprints to seed your database
+| with dummy data.
+|
+*/
+
+const Factory = use('Factory')
+
+Factory.blueprint('App/Models/Author', (faker, index, data) => {
+  const defaultValue = {
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    birthday: faker.date.between('1450-01-01', '2010-12-31')
+  }
+
+  return Object.assign(defaultValue, data)
+})
