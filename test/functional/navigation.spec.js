61a8a6af4d76453bb5ed570c2f34f3ab4fd73bb2
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-06-23 14:22:38 -0300

    Add teste funcional navegação

---
+++ test/functional/navigation.spec.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('Navegação')
+
+trait('Test/Browser')
+
+test('Deve mostrar a opção de autores', async ({ browser }) => {
+  const page = await browser.visit('/')
+  await page.assertHasIn('ul.navigation-list li:first-child a', 'Autores')
+})
+
+test('Deve mostrar a opção de editoras', async ({ browser }) => {
+  const page = await browser.visit('/')
+  await page.assertHasIn('ul.navigation-list li:nth-child(2) a', 'Editoras')
+})
+
+test('Deve mostrar a opção de livros', async ({ browser }) => {
+  const page = await browser.visit('/')
+  await page.assertHasIn('ul.navigation-list li:last-child a', 'Livros')
+})
