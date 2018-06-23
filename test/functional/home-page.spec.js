e50764897d27a2e00942e2a284e32ad0f275f7b7
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-06-23 14:22:26 -0300

    Add teste funcional home

--- test/functional/home-page.spec.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { test } = use('Test/Suite')('Home Page')
-
-test('Visit home page', async ({ browser }) => {
-  const page = await browser.visit('/')
-  await page.assertHas('Adonis')
-})
