628e59c4beb2dd40fdcda5b399580071aa9f2363
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-06-25 08:28:22 -0300

    Add teste funcional homepage

--- test/functional/home.spec.js
+++ test/functional/home.spec.js
@@ -1,10 +1,20 @@
 'use strict'
 
-const { test, trait } = use('Test/Suite')('Home')
+const { test, trait } = use('Test/Suite')('Página inicial')
 
 trait('Test/Browser')
 
-test('Visit home page', async ({ browser }) => {
+test('Deve mostrar o título', async ({ browser }) => {
   const page = await browser.visit('/')
-  await page.assertHas('Bookist')
+  await page.assertHasIn('h1.title', 'Bookist!')
+})
+
+test('Deve mostrar o sub-título', async ({ browser }) => {
+  const page = await browser.visit('/')
+  await page.assertHasIn('p.subtitle', 'Uma maneira simples de manter seu histórico de livros.')
+})
+
+test('Deve mostrar a dica', async ({ browser }) => {
+  const page = await browser.visit('/')
+  await page.assertHasIn('p.tip', 'use as opções acima para começar ;)')
 })
